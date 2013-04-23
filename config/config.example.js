e4f1d0f716b07f04644a7a58435394ad7657653b
Padraig O'Sullivan <osullivan.padraig@gmail.com>
2013-04-23 18:43:04 -0400

    Update config example.

--- config/config.example.js
+++ config/config.example.js
@@ -1,35 +1,23 @@
 
 var path = require('path')
   , rootPath = path.normalize(__dirname + '/..')
-  , templatePath = path.normalize(__dirname + '/../app/mailer/templates')
-  , notifier = {
-      APN: false,
-      email: false, // true
-      actions: ['comment'],
-      tplPath: templatePath,
-      postmarkKey: 'POSTMARK_KEY',
-      parseAppId: 'PARSE_APP_ID',
-      parseApiKey: 'PARSE_MASTER_KEY'
-    }
 
 module.exports = {
   development: {
-    db: 'mongodb://localhost/noobjs_dev',
-    akiban: 'https://d8cfb154_6263_489f_9f34_e5b96b42ecdf:95731f320b789fbaf5e6@db1.akiban.com:8091/v1',
-    akibandb: 'postgres://d8cfb154_6263_489f_9f34_e5b96b42ecdf:95731f320b789fbaf5e6@db1.akiban.com:15432/d8cfb154_6263_489f_9f34_e5b96b42ecdf',
+    akiban: 'https://9f7205d9_9702_47ac_ac3e_dd780c7233ee:f6cb815a727d280798ef@db1.akiban.com:8091/v1',
+    akibandb: {
+      host: 'db1.akiban.com',
+      port: 15432,
+      username: '9f7205d9_9702_47ac_ac3e_dd780c7233ee',
+      password: 'f6cb815a727d280798ef',
+      database: '9f7205d9_9702_47ac_ac3e_dd780c7233ee'
+    },
     root: rootPath,
-    notifier: notifier,
     app: {
       name: 'Nodejs Express Akiban Demo'
     },
   },
   test: {
-    db: 'mongodb://localhost/noobjs_test',
-    root: rootPath,
-    notifier: notifier,
-    app: {
-      name: 'Nodejs Express Akiban Demo'
-    },
   },
   production: {}
 }
